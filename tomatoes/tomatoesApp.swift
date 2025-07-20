715686d8e9e06a712fb1db395caacab5a8815d99
feliciawiryadi <felicia.angie.w@gmail.com>
2025-07-20 17:00:36 +1000

    Initial Commit

---
+++ tomatoes/tomatoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomatoesApp.swift
+//  tomatoes
+//
+//  Created by Felicia Wiryadi on 20/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct tomatoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tomatoesTests/tomatoesTests.swift
@@ -0,0 +1,17 @@
+//
+//  tomatoesTests.swift
+//  tomatoesTests
+//
+//  Created by Felicia Wiryadi on 20/7/2025.
+//
+
+import Testing
+@testable import tomatoes
+
+struct tomatoesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
