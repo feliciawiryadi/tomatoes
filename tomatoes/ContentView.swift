8af7b1305c19e5732a57f1ecc935689313efe112
feliciawiryadi <felicia.angie.w@gmail.com>
2025-07-21 16:25:14 +1000

    Create basic visual appearance of the window, buttons are not working yet

--- tomatoes/ContentView.swift
+++ tomatoes/ContentView.swift
@@ -10,13 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Spacer()
+                Text("text field here")
+            }
+            Spacer()
+            HStack {
+                HStack {
+                    Button(action: {}) {
+                        Text("25m")
+                            .frame(width: 30)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Text("5m")
+                            .frame(width: 30)
+                    }
+                }
+                .frame(width: 100)
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Text("start")
+                        .frame(width: 50)
+                }
+                
+            }
         }
         .padding()
+        .frame(maxWidth: 300, maxHeight: 100)
+//        .cornerRadius(100.0)
     }
+        
 }
 
 #Preview {
